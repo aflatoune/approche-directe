d1c1937e0d3822d8936b86b6ef8356a971bb62e1
aflatoune <daniel.kharsa@gmail.com>
2021-07-21 15:09:11 +0200

    create graph method for etalonnage class

--- R/graph.R
+++ R/graph.R
@@ -2,6 +2,9 @@ graph <- function(x, ...) {
     UseMethod("graph", x)
 }
 
+graph.default <- function(x, ...) {
+    stop("Method is not implemented for this class of object.")
+}
 
 graph.etalonnage <- function(x, ...) {
     if (!inherits(x, "etalonnage")) {
