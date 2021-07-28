d31ed86934aaedf7f769a0d0e78a2569e8518475
aflatoune <daniel.kharsa@gmail.com>
2021-07-28 15:59:45 +0200

    update doc

--- R/extend_series.R
+++ R/extend_series.R
@@ -7,7 +7,6 @@
 #' @param cols A vector of characters indicating columns to extend.
 #' @param n An integer indicating the number of samples to remove and predict.
 #' TODO: generalize to n > 1
-#' TODO: select cols with dplyr selection helpers
 #'
 #' @return A df/tibble
 #' @export
