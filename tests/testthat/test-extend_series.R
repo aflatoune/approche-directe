7887462099d74c15030a17eaaa46ccad3d1104f6
aflatoune <daniel.kharsa@gmail.com>
2021-12-02 11:06:15 +0100

    update param names

--- tests/testthat/test-extend_series.R
+++ tests/testthat/test-extend_series.R
@@ -7,7 +7,9 @@ test_that("extend_series returns same results as a direct call to forecast::auto
                               b = runif(50, min = 0, max = 5),
                               c = 1:50)
               x_extended <-
-                  x %>% extend_series(cols = c("a", "b"), n = c(1, 2))
+                  x %>% extend_series(columns = c("a", "b"),
+                                      n = c(1, 2),
+                                      mode = "ARIMA")
               arima_a <- forecast::auto.arima(x$a[-50],
                                               max.p = 4,
                                               max.q = 4,
@@ -17,7 +19,8 @@ test_that("extend_series returns same results as a direct call to forecast::auto
                                               max.q = 4,
                                               max.d = 1)
               A <- c(x$a[-50], predict(arima_a, n.ahead = 1)$pred)
-              B <- c(x$b[-c(49, 50)], predict(arima_b, n.ahead = 2)$pred)
+              B <-
+                  c(x$b[-c(49, 50)], predict(arima_b, n.ahead = 2)$pred)
               expect_equal(A, x_extended$a)
               expect_equal(B, x_extended$b)
           })
