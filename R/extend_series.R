87461dcdf746d74c23df8c701ddf74ce03c8cbab
aflatoune <daniel.kharsa@gmail.com>
2021-12-02 11:05:51 +0100

    extend using last observed value

--- R/extend_series.R
+++ R/extend_series.R
@@ -1,34 +1,49 @@
-#' Extend series with the best ARIMA(p,d,q) model
+#' Extend series
 #'
 #' This function relies on forecast::auto.arima.
 #' See <https://www.rdocumentation.org/packages/forecast/versions/8.15/topics/auto.arima>
 #'
 #' @param X A df/tibble.
-#' @param cols A vector of characters indicating columns to extend.
+#' @param columns A vector of characters indicating columns to extend.
 #' @param n An vector indicating the number of samples to remove and predict for
-#' each column name in `cols`. Be careful to respect the order in `cols`.
+#' each column name in `columns`. Be careful to respect the order in `columns`.
+#' @param mode Indicates to extend columns using an ARMA(p,d,q) model or by
+#' replacing missing values with the last observed value .
+#' Must be one of  `"ARIMA"` or `"constant"`.
 #'
 #' @return A df/tibble
 #' @export
-extend_series <- function(X, cols, n) {
-    if (!identical(length(cols), length(n))) {
-        stop("\"cols\" and \"n\" must have the same length.")
-    }
+extend_series <-
+    function(X, columns, n, mode = c("ARIMA", "constant")) {
+        if (!identical(length(columns), length(n))) {
+            stop("\"columns\" and \"n\" must have the same length.")
+        }
+        mode <- match.arg(mode)
+        n_max <- dim(X)[1]
+        for (i in seq_along(columns)) {
+            col = columns[i]
+            serie <- X %>%
+                dplyr::pull(col)
+            serie <- serie[-c((n_max - n[i] + 1):n_max)]
 
-    n_max <- dim(X)[1]
-    for (i in seq_along(cols)) {
-        col = cols[i]
-        serie <- X %>%
-            dplyr::pull(col)
-        serie <- serie[-c((n_max - n[i] + 1):n_max)]
-        arima_model <-
-            forecast::auto.arima(serie,
-                                 max.p = 4,
-                                 max.q = 4,
-                                 max.d = 1)
-        serie[(n_max - n[i] + 1):n_max] <- predict(arima_model, n.ahead = n[i])$pred
-        X <- X %>%
-            dplyr::mutate({{ col }} := serie)
+            if (identical(mode, "ARIMA")) {
+                arima_model <-
+                    forecast::auto.arima(y = serie,
+                                         max.p = 4,
+                                         max.q = 4,
+                                         max.d = 1)
+                serie[(n_max - n[i] + 1):n_max] <-
+                    forecast::forecast(arima_model, h = n[i])$mean
+            } else if (identical(mode, "constant")) {
+                serie[(n_max - n[i] + 1):n_max] <- serie[length(serie)]
+            }
+
+            X <- X %>%
+                dplyr::mutate({
+                    {
+                        col
+                    }
+                } := serie)
+        }
+        return(X)
     }
-    return(X)
-}
